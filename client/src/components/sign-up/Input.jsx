be5dc3be87170e2588a4481565d8d7f2616c2acd
Khánh Huân <www.khanhhuan@gmail.com>
2024-12-26 16:04:41 +0700

    Add: Sign up form with formik, validation

---
+++ client/src/components/sign-up/Input.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import { ErrorMessage, useField } from "formik";
+
+const FormInput = ({ name, ...rest }) => {
+  const [field] = useField(name);
+  return (
+    <div>
+      <input
+        name={name}
+        {...rest}
+        {...field}
+        className="p-3 rounded-lg border w-full"
+      />
+      <ErrorMessage name={name} component="div" className="text-red-500" />
+    </div>
+  );
+};
+
+FormInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+};
+
+export default FormInput;
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Formik } from "formik";
+import FormInput from "../components/sign-up/Input";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  username: Yup.string()
+    .required("Username is required")
+    .min(6, "Username must be at least 6 characters"),
+  email: Yup.string().email().required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
+    ),
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match")
+    .required("Confirm password is required"),
+});
+
+const initialValues = {
+  username: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
 const SignUp = () => {
-  return <div></div>;
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div className="mx-auto max-w-lg p-4">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <Formik
+        validationSchema={schema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
+        <Form className="flex flex-col gap-4">
+          <FormInput name="username" placeholder="Enter your username" />
+          <FormInput name="email" placeholder="Enter your email" />
+          <FormInput name="password" placeholder="Enter your password" />
+          <FormInput
+            name="confirmPassword"
+            placeholder="Confirm your password"
+          />
+          <button className="bg-slate-700 text-white p-3 rounded-lg font-bold hover:opacity-95">
+            Sign Up
+          </button>
+        </Form>
+      </Formik>
+      <div className="py-4">
+        <Link
+          to="/sign-in"
+          className="text-slate-700 opacity-80 hover:underline"
+        >
+          Have an account? Sign in
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
